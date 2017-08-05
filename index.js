f3f1b4ccd1f9ca89500526a73a9b10a538173f0d
Ryan Gaus <rsg1egoman@gmail.com>
2017-08-05 19:58:50 -0400

    chore: move all code into a main function
    - Move code into a main function
    - Call that main function when the file is called directly

--- index.js
+++ index.js
@@ -3,82 +3,47 @@ const {tokenizer, rewriter, generator} = require('./backend');
 const childProcess = require('child_process');
 const chalk = require('chalk');
 
-function getBranches() {
-  return new Promise((resolve, reject) => {
-    childProcess.exec('git branch', (err, stderr, stdout) => {
-      if (err) {
-        reject(err);
+const {getBranches, getRemotes, currentBranch} = require('./helpers/getEntities');
+
+function main() {
+  return Promise.all([
+    getBranches(childProcess),
+    getRemotes(childProcess),
+    currentBranch(childProcess),
+  ]).then(([branches, remotes, currentBranch]) => {
+    // Create a list of entities
+    const entities = [
+      ...branches.map(name => ({type: 'branch', name})),
+      ...remotes.map(name => ({type: 'remote', name})),
+    ];
+
+    // Parse the input
+    const input = process.argv.slice(2).join(' ');
+    const tokens = tokenizer(input);
+    const rewritten = rewriter(entities, tokens);
+    const output = generator(rewritten, currentBranch);
+
+    console.log(`Running $ ${chalk.cyan(`git ${output}`)}`);
+
+    // Spawn the git command, and execute:
+    const spawned = childProcess.spawn('git', output.split(' '), {cwd: process.cwd()});
+    spawned.stdout.on('data', d => console.log(d.toString()));
+    spawned.stderr.on('data', d => console.log(d.toString()));
+
+    spawned.on('close', code => {
+      if (code === 0) {
+        console.log(chalk.green(`Successfully pushed.`));
       } else {
-        const branches = `${stderr}${stdout}`.split('\n').map(b => b.slice(2)).filter(b => b.length);
-        resolve(branches);
+        process.exit(code);
       }
     });
   });
 }
 
-function getRemotes() {
-  return new Promise((resolve, reject) => {
-    childProcess.exec('git remote', (err, stderr, stdout) => {
-      if (err) {
-        reject(err);
-      } else {
-        const remotes = `${stderr}${stdout}`.split('\n')
-        resolve(remotes);
-      }
-    });
+if (require.main === module) {
+  main().catch(err => {
+    console.error(chalk.red('Error running command:'));
+    console.error(err);
+    process.exit(-1);
   });
 }
-
-function currentBranch() {
-  return new Promise((resolve, reject) => {
-    childProcess.exec('git branch', (err, stderr, stdout) => {
-      if (err) {
-        reject(err);
-      } else {
-        const branch = `${stderr}${stdout}`.split('\n').find(b => b.startsWith('*'))
-        resolve(branch.slice(2));
-      }
-    });
-  });
-}
-
-Promise.all([
-  getBranches(),
-  getRemotes(),
-  currentBranch(),
-]).then(([
-  branches,
-  remotes,
-  currentBranch,
-]) => {
-  // Create a list of entities
-  const entities = [
-    ...branches.map(name => ({type: 'branch', name})),
-    ...remotes.map(name => ({type: 'remote', name})),
-  ];
-
-  // Parse the input
-  const input = process.argv.slice(2).join(' ');
-  const tokens = tokenizer(input);
-  const rewritten = rewriter(entities, tokens);
-  const output = generator(rewritten, currentBranch);
-
-  console.log(`Running $ ${chalk.cyan(`git ${output}`)}`);
-
-  // Spawn the git command, and execute:
-  const spawned = childProcess.spawn('git', output.split(' '), {cwd: process.cwd()});
-  spawned.stdout.on('data', d => console.log(d.toString()));
-  spawned.stderr.on('data', d => console.log(d.toString()));
-
-  spawned.on('close', code => {
-    if (code === 0) {
-      console.log(chalk.green(`Successfully pushed.`));
-    } else {
-      process.exit(code);
-    }
-  });
-}).catch(err => {
-  console.error(chalk.red('Error running command:'));
-  console.error(err);
-  process.exit(-1);
-})
